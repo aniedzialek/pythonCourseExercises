4850d659cd1a4e0a2ed9cb135c641a9d7e82a576
Anna Niedzialek <anna.m.niedzialek@gmail.com>
2017-12-16 22:19:12 +0100

    Section 2 Lecture 12

--- 001_Section1_Lecture5.py
+++ 001_Section1_Lecture5.py
@@ -1,2 +1,4 @@
-greeting = input("Input your greeting message: ")
-print(greeting)
+age = input("What's your age: ")
+year = 2017
+bornYear = year - int(age)
+print("You were born in ", str(bornYear))
