fedf401765fab78405a9a2438edb26aa4eaaace8
Anna Niedzialek <anna.m.niedzialek@gmail.com>
2017-12-17 21:11:18 +0100

    Section 2 Lecture 18 - dictionaries

---
+++ 005_Begins-Section2_Lecture18-dictionaries.py
@@ -0,0 +1,4 @@
+someStuff = {"Name":"Anna", "Number":"7", "Cities":("Toronto", "Malaga", "Berlin", 2)}      # dictionary
+print(someStuff["Cities"][2])
+
+# dictonaries are unordered in contrast to lists and tupples which are ordered
