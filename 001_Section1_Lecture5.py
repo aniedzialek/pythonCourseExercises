fea4343cb9de06f7e96a79c89d812645805422af
Anna Niedzialek <anna.m.niedzialek@gmail.com>
2017-12-17 19:20:47 +0100

    Section 2 Lecture 14

--- 001_Section1_Lecture5.py
+++ 001_Section1_Lecture5.py
@@ -2,3 +2,6 @@ age = input("What's your age: ")
 year = 2017
 bornYear = year - int(age)
 print("You were born in ", str(bornYear))
+# in active mode use:
+## help(<type>.method) to get information about a method, example: help("").replace)
+## dir(<type>) to get information about methods of the type
