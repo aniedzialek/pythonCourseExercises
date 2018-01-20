d77b3156f851fa7b72998caec1e5273ee4b1ccdc
Anna Niedzialek <anna.m.niedzialek@gmail.com>
2018-01-20 14:01:31 +0100

    Lecture 50 - for loop with multiple lists

--- 010_Begins-Section5_Lecture43-Loops.py
+++ 010_Begins-Section5_Lecture43-Loops.py
@@ -17,3 +17,11 @@ while password != expectedPassword:
         print('You are logged in!')
     else:
         print('Sorry, try again')
+
+# --------------------------------
+# Multiple lists
+names = ['Tim', 'Jerry', 'Elvis']
+email_domains = ['gmail', 'hotmail', 'yahoo']
+
+for i,j in zip(names, email_domains):
+    print(i,j)
